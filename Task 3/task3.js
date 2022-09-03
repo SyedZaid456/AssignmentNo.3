06490dded4233d58d7b5462d3dede19a7ebaf55e
SyedZaid456 <111638799+SyedZaid456@users.noreply.github.com>
2022-09-03 15:55:24 +0500

    Update task3.js
    update

--- Task 3/task3.js
+++ Task 3/task3.js
@@ -1,21 +1,3 @@
-function Integers() {
-  let firstInteger = prompt("This is the first integer");
-  let secondInteger = prompt("The second integer is");
-  if (firstInteger > secondInteger) {
-    document.write(
-      `<h1>This is the largest of the two integers: ${firstInteger} </h1>`
-    );
-    return firstInteger;
-  } else if (firstInteger < secondInteger) {
-    document.write(
-      `<h1>This is the largest of the two integers: ${secondInteger} </h1>`
-    );
-    return secondInteger;
-  } else {
-    document.write("<h1>The integers are equal</h1>");
-  }
-}
-Integers();
 function integer(a, b) {
   if (a >= 0 && b >= 0) {
     if (a != b) {
@@ -28,4 +10,4 @@ function integer(a, b) {
   }
 }
 
-integer(10, 10);
+integer(8, 10);
